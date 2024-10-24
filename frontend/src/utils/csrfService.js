e8c3e6e1fe03d32b88f5e71cdec6f0bec8fb6633
caleb kibe <kiplagatcaleb2@gmail.com>
2024-10-24 11:44:55 +0300

    session tokenisation

---
+++ frontend/src/utils/csrfService.js
@@ -0,0 +1,8 @@
+// utils/csrfService.js
+export function getCSRFToken() {
+    const csrfToken = document.cookie
+      .split('; ')
+      .find(row => row.startsWith('csrftoken='))
+      ?.split('=')[1];
+    return csrfToken;
+  }
